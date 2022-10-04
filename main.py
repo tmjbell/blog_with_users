e6ca4f7cab54505fdc6297a2da93d2366370a77f
tmjbell <80283172+tmjbell@users.noreply.github.com>
2022-10-04 13:42:22 -0600

    db_testing_commit

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ from os import getenv
 from dotenv import load_dotenv
 
 
-load_dotenv("C:/Users/Tim/PycharmProjects/secrets.env")
-secret = getenv("FLASK_SECRET")
-db_url = getenv("SQL_DB")
+FLASK_SECRET='8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+SQL_DB='sqlite:///blog.db'
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret
+app.config['SECRET_KEY'] = FLASK_SECRET
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_DATABASE_URI'] = SQL_DB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
